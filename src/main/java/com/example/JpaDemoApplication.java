e7c4405320480c81dd6007f752118e340478babf
Ranzes33 <ranzes33@gmail.com>
2022-01-04 16:27:10 -0500

    Anotacion one to one

--- src/main/java/com/example/JpaDemoApplication.java
+++ src/main/java/com/example/JpaDemoApplication.java
@@ -1,11 +1,16 @@
 package com.example;
 
 import com.example.model.Categoria;
+import com.example.model.Vacante;
 import com.example.repository.CategoriasRepository;
+import com.example.repository.VacantesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -16,7 +21,10 @@ import java.util.Optional;
 public class JpaDemoApplication implements CommandLineRunner {
 
 	@Autowired
-	private CategoriasRepository repo;
+	private CategoriasRepository repoCategorias;
+
+	@Autowired
+	private VacantesRepository repoVacantes;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
@@ -24,21 +32,22 @@ public class JpaDemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		guardar();
+		buscarVacantes();
 	}
+	//metodos de crud repository
 	private void modificar(){
-		Optional<Categoria> optional = repo.findById(1);
+		Optional<Categoria> optional = repoCategorias.findById(1);
 		if(optional.isPresent()){
 			Categoria catTmp= optional.get();
 			catTmp.setNombre("Ing. de software");
 			catTmp.setDescripcion("Desarrollo de sistemas");
-			repo.save(catTmp);
+			repoCategorias.save(catTmp);
 			System.out.println(optional.get());
 		}
 		else System.out.println("Cateooria no encontrada");
 	}
 	private void buscarPorId(){
-		Optional<Categoria> optional = repo.findById(1);
+		Optional<Categoria> optional = repoCategorias.findById(1);
 		if(optional.isPresent()) System.out.println(optional.get());
 		else System.out.println("Cateooria no encontrada");
 	}
@@ -47,13 +56,13 @@ public class JpaDemoApplication implements CommandLineRunner {
 		ids.add(1);
 		ids.add(4);
 		ids.add(10);
-		Iterable<Categoria> categorias = repo.findAllById(ids);
+		Iterable<Categoria> categorias = repoCategorias.findAllById(ids);
 		for(Categoria cat : categorias){
 			System.out.println(cat);
 		}
 	}
 	private void buscarTodos(){
-		Iterable<Categoria> categorias = repo.findAll();
+		Iterable<Categoria> categorias = repoCategorias.findAll();
 		for (Categoria cat :categorias){
 			System.out.println(cat);
 		}
@@ -62,24 +71,63 @@ public class JpaDemoApplication implements CommandLineRunner {
 		Categoria cat = new Categoria();
 		cat.setNombre("Finanzas");
 		cat.setDescripcion("Trabajos relacionados con finanzas y seguridad");
-		repo.save(cat);
+		repoCategorias.save(cat);
 		System.out.println("cat = " + cat);
 	}
 	private void conteo(){
-		long count= repo.count();
+		long count= repoCategorias.count();
 		System.out.println("total categorias = " + count);
 	}
 	private void eliminar(){
 		int idCategoria = 1;
-		repo.deleteById(idCategoria);
+		repoCategorias.deleteById(idCategoria);
 		System.out.println("Eliminando un registro");
 	}
 	private void eliminarTodos(){
-		repo.deleteAll();
+		repoCategorias.deleteAll();
 	}
 	private void existeId(){
-		boolean existe = repo.existsById(5);
+		boolean existe = repoCategorias.existsById(5);
 		System.out.println("La categorias existe?: " + existe);
 	}
 
+	//metodos de jpaRepository
+	private void buscarTodosJPA(){
+		List<Categoria> categorias = repoCategorias.findAll();
+		for(Categoria c : categorias){
+			System.out.println(c.getId() + " " + c.getNombre());
+		}
+	}
+	private void borrarTodoEnBloque(){
+		repoCategorias.deleteAllInBatch();
+	}
+	private void buscarTodosOrdenados(){
+		List<Categoria> categorias = repoCategorias.findAll(Sort.by("nombre").descending());
+		for(Categoria c : categorias){
+			System.out.println(c.getId() + " " + c.getNombre());
+		}
+	}
+	private void buscarTodosPaginacion(){
+		Page<Categoria> page = repoCategorias.findAll(PageRequest.of(2,6));
+		System.out.println("Total de registros: " + page.getTotalElements());
+		System.out.println("Total de paginas: " + page.getTotalPages());
+		for (Categoria c : page.getContent()){
+			System.out.println(c.getId() + " " +c.getNombre());
+		}
+	}
+	private void buscarTodosPaginacionOrdenados(){
+		Page<Categoria> page = repoCategorias.findAll(PageRequest.of(0,5, Sort.by("nombre").descending()));
+		System.out.println("Total de registros: " + page.getTotalElements());
+		System.out.println("Total de paginas: " + page.getTotalPages());
+		for (Categoria c : page.getContent()){
+			System.out.println(c.getId() + " " +c.getNombre());
+		}
+	}
+
+	private void buscarVacantes(){
+		List<Vacante> lista = repoVacantes.findAll();
+		for (Vacante vacante: lista) {
+			System.out.println(vacante);
+		}
+	}
 }
--- src/main/java/com/example/repository/CategoriasRepository.java
+++ src/main/java/com/example/repository/CategoriasRepository.java
@@ -1,8 +1,10 @@
 package com.example.repository;
 
 import com.example.model.Categoria;
-import org.springframework.data.repository.CrudRepository;
 
-public interface CategoriasRepository extends CrudRepository<Categoria, Integer> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+//public interface CategoriasRepository extends CrudRepository<Categoria, Integer>
+public interface CategoriasRepository extends JpaRepository<Categoria, Integer> {
 
 }
